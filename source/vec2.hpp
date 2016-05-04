180cf2257009548fda8b7a76ffff6ac2001eba1a
Daniel Liebmann <nelu8763@billl05.in.uni-weimar.de>
2016-05-04 16:08:36 +0200

    Add Vec2

---
+++ source/vec2.hpp
@@ -0,0 +1,25 @@
+#ifndef VEC2_HPP
+#define VEC2_HPP 
+//wenn mehrere Module die gleiche Bibliothek benutzen; Guard verhindert Fehler
+//Destruktor: wird aufgerufen wenn Objekt der Klasse zerstoert wird
+//Benoetigt keinen, weil dieser automatisch angelegt wird(durch Compiler), wenn er verwendet werden wuerde
+//Fuer Aktion beim loeschen eines Objektes
+// Vec2 class definition
+class Vec2
+{
+public:
+	Vec2() : x(0), y(0) {};
+	Vec2(float x, float y) : x(x), y(y) {};
+	
+	Vec2& operator+=(Vec2 const& v);
+	Vec2& operator-=(Vec2 const& v);
+	Vec2& operator*=(float v);
+	Vec2& operator/=(float v);
+
+
+	float x;
+	float y;
+};
+
+#endif // VEC2_HPP
+	
